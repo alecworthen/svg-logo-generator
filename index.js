b7bb7a7e02596622342ca31399add10a92b89ea3
Alec Worthen <alecworthen@gmail.com>
2023-11-28 20:19:05 -0800

    updated code

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { Circle, Triangle, Square } = require('./main/lib/options')
+const { Circle, Triangle, Square } = require('./main/lib/shapes');
+
+class svg{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+    
+        return `<svg>${this.shapeElement}${this.textElement}</svg>`
+    };
+    setTextElement(text,color){
+        this.textElement = `<text>${text}</text>`
+        
+    }
+}
